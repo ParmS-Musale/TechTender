19999a546439d75a1d55dacb8df8750182f6afc5
ParmS-Musale <musaleparm9541@gmail.com>
2024-10-10 13:23:52 +0530

    Data Sanitization & Schema Validation..!!

--- src/models/user.js
+++ src/models/user.js
@@ -1,47 +1,58 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema(
   {
     firstName: {
       type: String,
-      required:true,
-      minLength:4,
-      maxLength:20,
+      required: true,
+      minLength: 4,
+      maxLength: 20,
     },
     lastName: {
       type: String,
     },
     emailId: {
       type: String,
-      required:true,
-      unique:true,
-      trim : true,
+      required: true,
+      unique: true,
+      trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Email is not valid");
+        }
+      },
     },
     age: {
       type: Number,
-      min :18 ,
+      min: 18,
     },
     gender: {
       type: String,
-      validate(value){
-        if(!["male", "female" ,"others"].includes(value)){
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
           throw new Error("Gender should be male, female or others");
         }
-      }
+      },
     },
     password: {
       type: String,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Enter a Strong Password");
+        }
+      },
     },
     skills: {
       type: [String],
     },
     about: {
       type: String,
-      default : "This is the default description",
+      default: "This is the default description",
     },
     photoUrl: {
       type: String,
-      default : "This is the default image",
+      default: "This is the default image",
     },
   },
   {
