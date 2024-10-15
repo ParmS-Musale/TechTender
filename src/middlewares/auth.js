fa93d185d767a38e003b7b0e019c7a9650564929
ParmS-Musale <musaleparm9541@gmail.com>
2024-10-15 16:10:26 +0530

    Auth Middleware & Schema Methods..!!

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,23 +1,30 @@
-const adminAuth = (req, res, next) => {
-  console.log("Admin Auth is Checking...");
-  const token = "ParmDev";
-  const isAdminAuthorized = token === "ParmDev";
-  if (!isAdminAuthorized) {
-    res.status(401).send("unauthorized access");
-  } else {
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/user");
+
+const userAuth = async (req, res, next) => {
+  try {
+    const { token } = req.cookies;
+
+    if (!token) {
+      throw new Error("Token is not valid!!!!!!!!!");
+    }
+
+    const decodedObj = await jwt.verify(token, "DEV@Tinder$790");
+
+    const { _id } = decodedObj;
+
+    const user = await User.findById(_id);
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+    req.user = user;
+
     next();
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
   }
 };
 
-const userAuth = (req, res, next) => {
-  console.log("User Auth is Checking...");
-  const token = "ParmDev";
-  const isAdminAuthorized = token === "ParmDev";
-  if (!isAdminAuthorized) {
-    res.status(401).send("unauthorized access");
-  } else {
-    next();
-  }
-};
-
-module.exports = { adminAuth, userAuth };
+module.exports = { userAuth };
